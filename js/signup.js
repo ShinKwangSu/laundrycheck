78bf8ea5b0436c2d196759536b3fc8b6bf5d45a1
ShinKwangSu <ksya1999@naver.com>
2022-05-12 04:00:51 +0900

    회원가입 최종

--- js/signup.js
+++ js/signup.js
@@ -5,19 +5,18 @@ const pwCheckEl = document.querySelector('#pw-check')
 const phoneEl = document.querySelector('#phone')
 const btnEl = document.querySelector('button')
 const spanEl = document.querySelector('span')
+const form = document.querySelector('form')
 
 function pwCheck() {
   if (pwEl.value != "" && pwCheckEl.value != "") {
     if (pwEl.value == pwCheckEl.value) {
       spanEl.classList.remove('different')
       spanEl.textContent = '비밀번호가 일치합니다.'
-      btnEl.disabled = false
       return true
     }
     else if (pwEl.value != pwCheckEl.value) {
       spanEl.classList.add('different')
       spanEl.textContent = '비밀번호가 일치하지 않습니다.'
-      btnEl.disabled = true
       return false
     }
   }
@@ -26,22 +25,22 @@ function pwCheck() {
   }
 }
 
-function onlyNum(event, type) {
-  if (type == "numbers") {
-    if (event.keyCode < 48 || event.keyCode > 57) {
-      return false
-    }
+// 한글 입력 방지 -> 한글은 삭제
+idEl.onkeyup = function() {
+  var v = this.value;
+  this.value = v.replace(/[^a-z0-9]/gi, '');
+}
+
+phoneEl.onkeyup = function() {
+  var regexp = /[^0-9]/gi;
+  this.onkeyup = function(e){
+    var v = this.value;
+    this.value = v.replace(regexp,'');
   }
 }
 
-function chkCharCode(e) {
-  const regExp = /[^0-9a-zA-Z]/g;
-  const ele = e.target;
-  if (regExp.test(ele.value)) {
-    ele.value = ele.value.replace(regExp, '');
-  }
-}
-
+// submit(회원가입 버튼) 시 유효성 체크
+// 추가적으로 비밀번호의 길이나 특수문자 포함 여부 등의 기능 넣을지 의논
 function createId() {
   if (nameEl.value == "") {
     nameEl.nextElementSibling.classList.add('warning')
@@ -73,7 +72,64 @@ function createId() {
       phoneEl.nextElementSibling.classList.remove('warning')
     }, 1500)
   }
-  else {
-    return true
+  else if (pwCheck()){
+    form.submit()
   }
 }
+
+/*
+// 상황별 입력 제한
+window.onload = function(){
+  engAndNumberFunc(document.getElementById("id"));
+  onlyNumberFunc(document.getElementById("phone1"));
+  onlyNumberFunc(document.getElementById("phone2"));
+  onlyNumberFunc(document.getElementById("phone3"));
+  onlyKorFunc(document.getElementById("name"));
+  onlyEngFunc(document.getElementById("location"));
+}
+
+//영문, 숫자만 입력가능한 keyup 이벤트 함수
+function engAndNumberFunc(t){
+  var regexp = /[^a-z0-9]/gi;
+  t.onkeyup = function(e){
+    var v = this.value;
+    this.value = v.replace(regexp,'');
+  }
+}
+
+//숫자만 입력가능한 keyup 이벤트 함수
+function onlyNumberFunc(t){
+  var regexp = /[^0-9]/gi;
+  t.onkeyup = function(e){
+    var v = this.value;
+    this.value = v.replace(regexp,'');
+  }
+}
+
+//영문만 입력가능한 keyup 이벤트 함수
+function onlyEngFunc(t){
+  var regexp = /[^a-z]/gi;
+  t.onkeyup = function(e){
+    var v = this.value;
+    this.value = v.replace(regexp,'');
+  }
+}
+
+//한글만 입력가능한 keyup 이벤트 함수
+function onlyKorFunc(t){
+  var regexp = /[a-z0-9]|[ \[\]{}()<>?|`~!@#$%^&*-_+=,.;:\"'\\]/g;
+  t.onkeyup = function(e){
+    var v = this.value;
+    this.value = v.replace(regexp,'');
+  }
+}
+
+//한글, 숫자만 입력가능한 keyup 이벤트 함수
+function korAndNumberFunc(t){
+  var regexp = /[^ㄱ-ㅎ가-힣0-9]/gi;
+  t.onkeyup = function(e){
+    var v = this.value;
+    this.value = v.replace(regexp,'');
+  }
+}
+*/
